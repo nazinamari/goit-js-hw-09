b76a6dda6958485c87c6c87cacaa35cfafcbdce3
nazinamari <93985053+nazinamari@users.noreply.github.com>
2024-02-11 17:39:35 +0000

    check

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -15,14 +15,17 @@ function createPromise(position, delay) {
 
 const formRef = document.querySelector('.form');
 
-formRef.addEventListener('submit', el => {
+formRef.addEventListener('submit', handleSubmit);
+
+function handleSubmit (el) {
+  el.preventDefault();
   const delayValue = parseInt(formRef.elements.delay.value);
   const stepValue = parseInt(formRef.elements.step.value);
   const amountValue = parseInt(formRef.elements.amount.value);
 
-  el.preventDefault();
   createPromises(delayValue, stepValue, amountValue);
-});
+  formRef.reset();
+};
 
 function createPromises(delay, step, amount) {
   for (let i = 0; i < amount; i++) {
@@ -33,6 +36,5 @@ function createPromises(delay, step, amount) {
       .catch(({ position, delay }) => {
         Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
       });
-    formRef.reset();
   }
 }
